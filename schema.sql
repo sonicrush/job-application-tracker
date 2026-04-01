f95e9f4d932aabf6fa68df5eb1f0d1530959086c
Saneec <crisman2005@gmail.com>
2026-04-01 19:28:39 -0400

    Implement schema.sql

---
+++ schema.sql
@@ -0,0 +1,42 @@
+-- Create Companies Table
+CREATE TABLE companies (
+    company_id INT PRIMARY KEY AUTO_INCREMENT
+    company_name VARCHAR(100) NOT NULL
+    industry VARCHAR(50)
+    website VARCHAR(200)
+    city VARCHAR(50)
+    state VARCHAR(50)
+    notes TEXT
+)
+CREATE TABLE jobs (
+    job_id INT PRIMARY KEY AUTO_INCREMENT,
+    company_id INT (FK -> companies),
+    job_title VARCHAR(100) NOT NULL,
+    job_type ENUM('Full-time','Part-time','Contract','Internship'),
+    salary_min INT, salary_max INT,
+    job_url VARCHAR(300),
+    date_posted DATE,
+    requirements JSON,
+    FOREIGN KEY company_id REFERENCES companies(company_id)
+)
+CREATE TABLE applications (
+    application_id INT PRIMARY KEY AUTO_INCREMENT,
+    job_id INT,
+    application_date DATE NOT NULL,
+    status ENUM('Applied','Screening','Interview','Offer','Rejected','Withdrawn'),
+    resume_version VARCHAR(50),
+    cover_letter_sent BOOLEAN,
+    interview_data JSON,
+    FOREIGN KEY job_id REFERENCES jobs(job_id)
+)
+CREATE TABLE contacts (
+    contact_id INT PRIMARY KEY AUTO_INCREMENT
+    company_id INT,
+    contact_name VARCHAR(100) NOT NULL,
+    title VARCHAR(100),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    linkedin_url VARCHAR(200),
+    notes TEXT,
+    FOREIGN KEY company_id REFERENCES companies(company_id)
+)
